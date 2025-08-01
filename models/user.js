a1dd4e4def5d3f02f299585d641ad7c70ea2c89a
Vaibhav Lohani <lohanivaibhav4@gmail.com>
2025-08-01 14:26:14 +0530

    V1- With Stateful Auth

--- models/user.js
+++ models/user.js
@@ -12,6 +12,12 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true,
+    },
+    role:{
+        type:String,
+        required:true,
+        enum:['user','admin'],
+        default:'user'
     }
 })
 const USER = mongoose.model('USER', userSchema)
